d60e99441c0b368e487f5d381bb1f57ccc8af3fe
Diyana Hristova <90700181+Cappricornia@users.noreply.github.com>
2023-10-30 22:00:41 +0200

    Update EssentialJavaConcepts

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import intro.java.NumbersOneToTen;
+
+public class Main {
+    public static void main(String[] args) {
+        NumbersOneToTen.numbersOneToTen(1);
+    }
+}
+
---
+++ src/intro/java/NumbersOneToTen.java
@@ -0,0 +1,10 @@
+package intro.java;
+
+public class NumbersOneToTen {
+    public static void numbersOneToTen (int number) {
+        if (number <= 10) {
+            System.out.println("The number is: " + number);
+            numbersOneToTen(number + 1);
+        }
+    }
+}
