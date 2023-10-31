076d52c1053e1a7aa5f627d24712a499109b4676
Diyana Hristova <90700181+Cappricornia@users.noreply.github.com>
2023-10-31 22:55:27 +0200

    Update EssentialJavaConcepts repo

---
+++ src/introduction/java/CalculateRectangleArea.java
@@ -0,0 +1,21 @@
+package introduction.java;
+import java.util.Scanner;
+
+public class CalculateRectangleArea {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Please enter the length of the rectangle: ");
+        double length = scanner.nextDouble();
+
+        System.out.print("Please enter the width of the rectangle: ");
+        double width = scanner.nextDouble();
+
+        double area = length * width;
+        String formattedArea = String.format("%.2f", area);
+
+        System.out.println("The area of the rectangle is: " + formattedArea + ".");
+
+        scanner.close();
+    }
+}
--- src/introduction/java/NumbersOneToTen.java
+++ src/introduction/java/NumbersOneToTen.java
@@ -3,7 +3,7 @@ package introduction.java;
 public class NumbersOneToTen {
     public static void numbersOneToTen (int number) {
         if (number <= 10) {
-            System.out.println("The number is => " + number);
+            System.out.println("The number is: " + number);
             numbersOneToTen(number + 1);
         }
     }
