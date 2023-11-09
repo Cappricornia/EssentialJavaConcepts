e3c1697948f69fa9eaebaab98ff3179c8ad6cdea
Diyana Hristova <90700181+Cappricornia@users.noreply.github.com>
2023-11-09 23:04:55 +0200

    Update introduction java

---
+++ src/conditional/statements/EvenOrOdd.java
@@ -0,0 +1,23 @@
+package conditional.statements;
+import java.util.Scanner;
+
+public class EvenOrOdd {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Please enter a whole number: ");
+
+        if (scanner.hasNextInt()) {
+            int number = scanner.nextInt();
+            if (number % 2 == 0) {
+                System.out.println("The number " + number + " is even.");
+            } else {
+                System.out.println("The number " + number + " is odd.");
+            }
+        } else {
+            System.out.println("Invalid input. Please enter a whole number without decimal points.");
+        }
+
+        scanner.close();
+    }
+}
