79c4edc29b3b99a097fd5f96f2642eb19f995c5e
Diyana Hristova <90700181+Cappricornia@users.noreply.github.com>
2023-11-24 08:44:22 +0200

    Update for loop examples

--- src/forloopexamples/SumChecker.java
+++ src/forloopexamples/SumChecker.java
@@ -5,31 +5,32 @@ public class SumChecker {
     public static void main(String[] args) {
         //Find the maximum number and check if the sum is equal to the maximum number.
         Scanner scanner = new Scanner(System.in);
-        int size = 0;
+        int N = 0;
         int maxNum = Integer.MIN_VALUE;
         int sum = 0;
 
-        while (size < 2 || size > 10) {
-            System.out.print("Please enter the size of the array (between 2 and 10): ");
+        // validate user input for N
+        while (N < 2 || N > 10) {
+            System.out.print("Please enter a whole number between 2 and 10: ");
 
             if (scanner.hasNextInt()) {
-                size = scanner.nextInt();
+                N = scanner.nextInt();
 
-                if (size < 2 || size > 10) {
-                    System.out.println("Error: Please enter a valid size between 2 and 10.");
+                if (N < 2 || N > 10) {
+                    System.out.println("Error: Please enter a valid whole number between 2 and 10.");
                 }
             } else {
-                System.out.println("Error: Please enter a valid size between 2 and 10.");
+                System.out.println("Error: Please enter a valid whole number between 2 and 10.");
                 scanner.next(); // Clear the non-integer input
             }
         }
 
-        System.out.println("You've entered a valid array size: " + size);
+        System.out.println("You've entered a valid whole number: " + N);
 
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < N; i++) {
             boolean isValidInput = false;
 
-            // validate use input
+            // validate user input for each number
             while (!isValidInput) {
                 System.out.print("Enter element " + (i + 1) + ": ");
 
